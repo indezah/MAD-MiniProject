087691c5ec3f3cad38d02a677630772176b7e26b
Nisura Indisa <nisuraindisa21@gmail.com>
2023-09-02 18:46:54 +0530

    Basic Navigation

--- app/src/main/java/com/mad/taskapolis/MainActivity.kt
+++ app/src/main/java/com/mad/taskapolis/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.mad.taskapolis
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val newFragment = NewFragment()
+        setCurrentFragment(homeFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    // TODO: Navigate to the home screen
+                    setCurrentFragment(homeFragment)
+                    true
+                }
+                R.id.search -> {
+                    // TODO: Navigate to the search screen
+                    setCurrentFragment(newFragment)
+                    true
+                }
+                R.id.profile -> {
+                    // TODO: Navigate to the profile screen
+                    true
+                }
+                else -> false
+            }
+        }
     }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
---
+++ app/src/main/java/com/mad/taskapolis/NewFragment.kt
@@ -0,0 +1,19 @@
+package com.mad.taskapolis
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_new, container, false)
+        return view
+    }
+}
